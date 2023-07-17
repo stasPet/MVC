e415187462941669b792becf2ea3f113d521ecda
Stanislav <petrov_stas@outlook.com>
2023-07-17 19:48:57 +0300

    t1

--- mvc.1/main.cpp
+++ mvc.1/main.cpp
@@ -1,14 +1,34 @@
 #include "boost/property_tree/xml_parser.hpp"
 #include "boost/property_tree/ptree.hpp"
 
-#include <istream>
+#include <fstream>
+
+using namespace boost::property_tree;
+
+struct MyTree
+{
+  std::string value;
+  std::vector< void * > nodes;
+
+  void parseXML( const ptree & pTree )
+  {
+    std::string v;
+    pTree.get_value(v);
+
+    for ( auto & r : pTree )
+    {
+      auto t{ r };
+    }
+  }
+};
 
 int main()
 {
-  using namespace boost::property_tree;
-
   ptree pTree;
   std::ifstream	iFStream{ R"(menuTree.xml)" };
 
   xml_parser::read_xml( iFStream, pTree );
+  
+  MyTree myTree;
+  myTree.parseXML( pTree );
 }
